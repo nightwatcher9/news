7f103e662bf038339fb5b568a4fbcdcc98ecc31f
守夜人 <871269725@qq.com>
2022-04-16 15:51:06 +0800

    news

---
+++ news/app/src/main/java/com/hjzf/ui/video/VideoFragment.kt
@@ -0,0 +1,35 @@
+package com.hjzf.ui.video
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.hjzf.databinding.FragmentVideoBinding
+
+class VideoFragment : Fragment() {
+
+    private val viewModel: VideoViewModel by viewModels()
+
+    private var _binding: FragmentVideoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentVideoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+}
---
+++ news/app/src/main/java/com/hjzf/ui/video/VideoViewModel.kt
@@ -0,0 +1,7 @@
+package com.hjzf.ui.video
+
+import androidx.lifecycle.ViewModel
+
+class VideoViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
